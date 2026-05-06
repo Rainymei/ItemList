f0bc95bfd4917d9cd5e2d0081853376ea89ded4c
alex <8379108+Alex33856@users.noreply.github.com>
2026-05-05 21:18:13 -0400

    Fix Creative Mode Issue, Add SkyBlock check

--- src/main/kotlin/com/operationpotato/itemlist/SkyblockItemList.kt
+++ src/main/kotlin/com/operationpotato/itemlist/SkyblockItemList.kt
@@ -7,7 +7,9 @@ import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents
 import net.fabricmc.fabric.api.client.screen.v1.ScreenMouseEvents
 import net.fabricmc.fabric.api.client.screen.v1.Screens
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import net.minecraft.client.gui.screens.inventory.InventoryScreen
 import org.slf4j.LoggerFactory
+import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.utils.extentions.right
 
 object SkyblockItemList : ClientModInitializer {
@@ -15,8 +17,10 @@ object SkyblockItemList : ClientModInitializer {
 
 	override fun onInitializeClient() {
 		logger.info("Hello Fabric world!")
-		ScreenEvents.AFTER_INIT.register { _, screen, w, h ->
+		ScreenEvents.AFTER_INIT.register { mc, screen, w, h ->
+			if (!LocationAPI.isOnSkyBlock) return@register
 			if (screen is AbstractContainerScreen<*>) {
+				if (screen is InventoryScreen && mc.player?.hasInfiniteMaterials() ?: false) return@register
 				val width = w - screen.right
 				val itemList = ItemList(screen.right, 0, width, h)
 
